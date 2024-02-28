4491ac28e98cbbdec0dd8595f26d87ab471dd29f
Harry <henry.iz@yandex.ru>
2024-02-28 12:31:16 +0300

    added the answer function

--- Bot.py
+++ Bot.py
@@ -1,14 +1,25 @@
 from telebot import TeleBot
+from tokens import TOKEN
 
-TOKEN = "7043913884:AAFg5FLvZeD9_VQbqcuFsmrnxuOZFnpk_d0"
 bot = TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id,
-                     "Hi! Type 'new task' if you want to create a new task.")
+                     "Hi!\nType 'new task' if you want to create a new task.\n"
+                     "Type 'todo list' if you want to look through your list todo.")
 
+@bot.message_handler(content_types=["text"])
+def answer(message):
+    if message.text == "new task":
+        bot.send_message(
+                        message.chat.id,
+                        "Enter the name of you task:")
+    if message.text == "todo list":
+        bot.send_message(
+                        message.chat.id,
+                        "Here will be you list:")
 
 bot.polling(non_stop=True, interval=0)
 
